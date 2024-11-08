32ef38f187625076bc95046e081e19af03cb4825
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-08 00:41:35 +0000

    Change environment variable name for secret key.

--- portfolio/environment_variables.py
+++ portfolio/environment_variables.py
@@ -5,4 +5,4 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(current_dir, '.env')
 load_dotenv(path)
 
-DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DJANGO_SECRET_KEY = os.getenv('PORTFOLIO_SECRET_KEY')
