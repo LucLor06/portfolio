f94fb934e41507a2a948e5300e7ae3c7d3ea5a81
LucLor06 <LukeLorenzBA@gmail.com>
2025-07-13 15:21:46 -0400

    feat(main.models): Add AbstractNamedModel
    AbstractNamedModel is an abstract model class for adding a name and slug field, which is generated from the name field.

--- main/models.py
+++ main/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 
 class User(AbstractUser):
     ...
+
+
+class AbstractNamedModel(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(editable=False)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if slugify(self.name) != self.slug:
+            self.slug = slugify(self.name)
+
+        return super().save(*args, **kwargs)
