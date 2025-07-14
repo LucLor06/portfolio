753a90c5381caa4338da5880a19c93581011ef14
LucLor06 <LukeLorenzBA@gmail.com>
2025-07-14 12:14:47 -0400

    feat(admin): Register models to the admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Language)
+admin.site.register(models.Technology)
+admin.site.register(models.Tool)
+admin.site.register(models.Skill)
+admin.site.register(models.Tag)
