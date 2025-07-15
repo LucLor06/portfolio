f7d013495756d91cc0873c88347157666422c6a4
LucLor06 <LukeLorenzBA@gmail.com>
2025-07-15 11:25:18 -0400

    feat(main.views): Add skills to home view context

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Language, Technology, Tool, Skill
 
 def home(request: HttpRequest):
-    return render(request, 'home.html')
+    context = {
+        'languages': Language.objects.all(),
+        'technologies': Technology.objects.all(),
+        'tools': Tool.objects.all(),
+        'skills': Skill.objects.all()
+    }
+    return render(request, 'home.html', context)
