0114d7eff188b94efc00711fa26fc0f369c63833
LucLor06 <LukeLorenzBA@gmail.com>
2025-07-17 13:34:58 -0400

    feat(views): Add projects to home view context

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from .models import Language, Technology, Tool, Skill
+from .models import Language, Technology, Tool, Skill, Project
 
 def home(request: HttpRequest):
     context = {
         'languages': Language.objects.all(),
         'technologies': Technology.objects.all(),
         'tools': Tool.objects.all(),
-        'skills': Skill.objects.all()
+        'skills': Skill.objects.all(),
+        'projects': Project.objects.all(),
     }
     return render(request, 'home.html', context)
