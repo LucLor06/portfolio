5e4444a111b77f7a572254f49c110ee85280d296
LucLor06 <lukelorenzba@gmail.com>
2025-07-19 21:05:11 -0500

    refactor(context-processors): Refactor website context processors to reflect CacheAll models

--- website/context_processors.py
+++ website/context_processors.py
@@ -4,7 +4,7 @@ from django.core.cache import cache
 def website(request):
     context = {
         'website': WebsiteConfig.get_solo(),
-        'website_socials': cache.get_or_set(WebsiteSocial._cache_key, lambda: list(WebsiteSocial.objects.all()), None),
-        'website_contacts': cache.get_or_set('website_contacts', lambda: list(WebsiteContact.objects.all()), None),
+        'website_socials': WebsiteSocial.objects.all(),
+        'website_contacts': WebsiteContact.objects.all(),
     }
     return context
