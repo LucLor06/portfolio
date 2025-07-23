456abde0cbb20b05d7f167d08c76ce132bb3d7dc
LucLor06 <lukelorenzba@gmail.com>
2025-07-22 19:44:06 -0500

    feat(environment-variables): Add DJANGO_DEBUG and PUBLIC_FACING_ADDRESS environment variables

--- environment_variables.py
+++ environment_variables.py
@@ -7,3 +7,5 @@ path = Path(__file__).resolve().parent / '.env'
 load_dotenv(path)
 
 DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+PUBLIC_FACING_ADDRESS = os.getenv('IP_ADDRESS')
+DJANGO_DEBUG = True if os.getenv('DJANGO_DEBUG') == 'True' else False
